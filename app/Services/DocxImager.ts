01cb2e94f87a9137b6a2c5c81531cf78668ff749
Dipanjan <dipanjan.panja@geogo.in>
2023-06-07 18:38:07 +0530

    update: code reformat

--- app/Services/DocxImager.ts
+++ app/Services/DocxImager.ts
@@ -1,71 +1,26 @@
 import axios from "axios";
-import https from "https";
-import { Transform } from "stream";
 
 var opts: any = {};
 opts.centered = false; //Set to true to always center images
 opts.fileType = "docx"; //Or pptx
 
-function base64DataURLToArrayBuffer(dataURL) {
-  const base64Regex = /^data:image\/(png|jpg|svg|svg\+xml);base64,/;
-  if (!base64Regex.test(dataURL)) {
-    return false;
-  }
-  const stringBase64 = dataURL.replace(base64Regex, "");
-  let binaryString;
-  // if (typeof window !== "undefined") {
-  //   binaryString = window.atob(stringBase64);
-  // } else {
-  binaryString = new Buffer(stringBase64, "base64").toString("binary");
-  // }
-  const len = binaryString.length;
-  const bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) {
-    const ascii = binaryString.charCodeAt(i);
-    bytes[i] = ascii;
-  }
-  return bytes.buffer;
-}
-
-export async function networkToBuffer(url) {
+export async function networkToBuffer(url: string) {
   try {
     const { data } = await axios.get(url, { responseType: "arraybuffer" });
-    // console.log("url", url);
-
-    // const res = await axios.get(url, {
-    //   responseType: "text",
-    //   responseEncoding: "base64",
-    // });
-
-    // const base64 = Buffer.from(res.data, "base64");
-    console.log(data);
-
     return data;
   } catch (error) {
-    console.log("error");
-    console.log(error);
+    console.log("networkToBuffer => ", error);
     return null;
   }
 }
 
 //Pass your image loader
-opts.getImage = async function (tagValue, tagName) {
+opts.getImage = async function (tagValue: string, _tagName: string) {
   //tagValue is 'examples/image.png'
   //tagName is 'image'
   // return fs.readFileSync(tagValue);
   // return base64DataURLToArrayBuffer(tagValue);
   return networkToBuffer(tagValue);
-  // return tagValue;
-  console.log(tagValue, tagName);
-  // tagValue is "https://docxtemplater.com/xt-pro-white.png" and tagName is "image"
-  // return new Promise(function (resolve, reject) {
-  //   getHttpData(tagValue, function (err, data) {
-  //     if (err) {
-  //       return reject(err);
-  //     }
-  //     resolve(data);
-  //   });
-  // });
 };
 
 opts.getSize = function (img, tagValue, tagName) {
@@ -85,31 +40,26 @@ opts.getSize = function (img, tagValue, tagName) {
   ];
 };
 
-function getHttpData(url, callback) {
-  https
-    .request(url, function (response) {
-      if (response.statusCode !== 200) {
-        return callback(
-          new Error(
-            `Request to ${url} failed, status code: ${response.statusCode}`
-          )
-        );
-      }
-
-      const data = new Transform();
-      response.on("data", function (chunk) {
-        data.push(chunk);
-      });
-      response.on("end", function () {
-        callback(null, data.read());
-      });
-      response.on("error", function (e) {
-        console.log(e);
-
-        callback(e);
-      });
-    })
-    .end();
+//@ts-ignore
+function base64DataURLToArrayBuffer(dataURL) {
+  const base64Regex = /^data:image\/(png|jpg|svg|svg\+xml);base64,/;
+  if (!base64Regex.test(dataURL)) {
+    return false;
+  }
+  const stringBase64 = dataURL.replace(base64Regex, "");
+  let binaryString;
+  // if (typeof window !== "undefined") {
+  //   binaryString = window.atob(stringBase64);
+  // } else {
+  binaryString = new Buffer(stringBase64, "base64").toString("binary");
+  // }
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    const ascii = binaryString.charCodeAt(i);
+    bytes[i] = ascii;
+  }
+  return bytes.buffer;
 }
 
 export default opts;
