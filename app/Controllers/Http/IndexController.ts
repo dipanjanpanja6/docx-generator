91c585a4291b3ce74ebc4aedc220c7bd384cf580
Soumik Maity <soumik.maity@geogo.in>
2023-06-07 13:17:16 +0530

    updated

--- app/Controllers/Http/IndexController.ts
+++ app/Controllers/Http/IndexController.ts
@@ -32,22 +32,71 @@ function base64DataURLToArrayBuffer(dataURL) {
   }
   return bytes.buffer;
 }
+
+async function networkToBuffer(url) {
+  try {
+    const { data } = await axios.get(url, {responseType: "arraybuffer"});
+    // console.log("url", url);
+
+    // const res = await axios.get(url, {
+    //   responseType: "text",
+    //   responseEncoding: "base64",
+    // });
+
+    // const base64 = Buffer.from(res.data, "base64");
+
+    return data;
+  } catch (error) {
+    console.log("error");
+    console.log(error);
+    return null;
+  }
+}
+
 //Pass your image loader
-opts.getImage = function (tagValue, tagName) {
+opts.getImage = async function (tagValue, tagName) {
   //tagValue is 'examples/image.png'
   //tagName is 'image'
   // return fs.readFileSync(tagValue);
-  return base64DataURLToArrayBuffer(tagValue);
+  // return base64DataURLToArrayBuffer(tagValue);
+  // return await networkToBuffer(tagValue);
+  return tagValue;
 };
 
 //Pass the function that return image size
+// opts.getSize = function (img, tagValue, tagName) {
+//   //img is the image returned by opts.getImage()
+//   //tagValue is 'examples/image.png'
+//   //tagName is 'image'
+//   //tip: you can use node module 'image-size' here
+//   return [150, 150];
+// };
+
 opts.getSize = function (img, tagValue, tagName) {
-  //img is the image returned by opts.getImage()
-  //tagValue is 'examples/image.png'
-  //tagName is 'image'
-  //tip: you can use node module 'image-size' here
-  return [150, 150];
-};
+  console.log(tagValue, tagName);
+  // img is the value that was returned by getImage
+  // This is to force the width to 600px, but keep the same aspect ratio
+  const sizeOf = require("image-size");
+  const sizeObj = sizeOf(img);
+  // console.log(sizeObj);
+  const forceWidth = 600;
+  const ratio = forceWidth / sizeObj.width;
+  console.log([
+    forceWidth,
+    Math.round(sizeObj.height * ratio),
+]);
+  
+  return [
+      forceWidth,
+      // calculate height taking into account aspect ratio
+      Math.round(sizeObj.height * ratio),
+  ];
+}
+
+
+
+
+
 
 export default class IndexController {
   private async load_images(
@@ -55,131 +104,24 @@ export default class IndexController {
     case_form_input_id: number,
     token: string
   ) {
-    const fetch_url = `${Env.get(
-      "API_HOST"
-    )}/api/case_cif_forms/${case_cif_form_id}/case_form_inputs/${case_form_input_id}`;
-    const headers = {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    };
-    var { data } = await axios.get(fetch_url, { headers });
-    return data;
+    try {
+      const fetch_url = `${Env.get(
+        "API_HOST"
+      )}/api/case_cif_forms/${case_cif_form_id}/case_form_inputs/${case_form_input_id}`;
+      const headers = {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      };
+      var { data } = await axios.get(fetch_url, { headers });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  // public async indexq({ response, request }: HttpContextContract) {
-  //   const case_cif_form_id = request.param("id");
-  //   // const { case_form_input_id, token, agent_id } = await request.validate({
-  //   //   schema: schema.create({
-  //   //     case_form_input_id: schema.number(),
-  //   //     token: schema.string(),
-  //   //     agent_id: schema.number(),
-  //   //   }),
-  //   // });
-
-  //   const resultFile = Application.publicPath(`uploads/result.docx`);
-  //   const templateFile = Application.publicPath("uploads/template_1.docx");
-  //   // const case_cif_form = await Database.query()
-  //   //   .from("case_cif_forms")
-  //   //   .where({ id: case_cif_form_id })
-  //   //   .firstOrFail();
-
-  //   // const case_form_input = await Database.query()
-  //   //   .from("case_form_inputs")
-  //   //   .where("case_cif_form_id", "=", case_cif_form.id)
-  //   //   .where("submitted_by", "=", agent_id)
-  //   //   .firstOrFail();
-
-  //   // const cif_form_template = await Database.query()
-  //   //   .from("cif_form_templates")
-  //   //   .where("case_cif_form_id", "=", case_cif_form.id)
-  //   //   .firstOrFail();
-
-  //   // const cif_form_field_templates = await Database.query()
-  //   //   .from("cif_form_field_templates")
-  //   //   .where("cif_form_template_id", "=", cif_form_template.id);
-
-  //   // var images = await this.load_images(
-  //   //   case_cif_form_id,
-  //   //   case_form_input_id,
-  //   //   token
-  //   // );
-
-  //   // let cif_inputs: any[] = [];
-  //   // cif_form_field_templates.map((t) => {
-  //   //   if (t.field_type === 7 && t.field_meta && t.field_meta.select_inputs) {
-  //   //     var obj = t.field_meta.select_inputs.find(
-  //   //       (ip: any) => ip.value == case_form_input.data[t.field_code]
-  //   //     );
-  //   //     cif_inputs.push({ label: `${t.field_label}`, value: obj.label });
-  //   //   } else {
-  //   //     cif_inputs.push({
-  //   //       label: `${t.field_label}`,
-  //   //       value: case_form_input.data[t.field_code],
-  //   //     });
-  //   //   }
-  //   // });
-
-  //   var data = {
-  //     // cif_inputs,
-  //     // insured_photos: images?.data?.insured_photos?.map((img: any) => img.url),
-  //     insured_photos: [
-  //       {
-  //         value: `{{insert_image img1 png 400 400}}`,
-  //       },
-  //       {
-  //         value: `{{insert_image img2 jpg 300 400}}`,
-  //       },
-  //     ],
-  //     // id_proofs: images?.data?.id_proofs?.map((img: any) => img.url),
-  //     // house_photos: images?.data?.house_photos?.map((img: any) => img.url),
-  //     // fe_selfies: images?.data?.fe_selfies?.map((img: any) => img.url),
-  //     // landmark_photos: images?.data?.landmark_photos?.map(
-  //     //   (img: any) => img.url
-  //     // ),
-  //     // evidence_photos: images?.data?.evidence_photos?.map(
-  //     //   (img: any) => img.url
-  //     // ),
-  //     // signature: images?.data?.signature.url,
-  //   };
-  //   // console.log(data);
-
-  //   var options = {
-  //     // if you want to convert this doc to pdf you have to install external packages, see carbon doc
-  //     //   convertTo: "pdf",
-  //   };
-  //   // update text placeholder
-  //   await new Promise((resolve, reject) => {
-  //     carbone.render(templateFile, data, options, function (err, result) {
-  //       if (err) {
-  //         reject(err);
-  //         return console.log(err);
-  //       }
-  //       // write the result
-  //       fs.writeFileSync(resultFile, result);
-  //       resolve(result);
-  //     });
-  //   });
-
-  //   // lets update image placeholder
-  //   let docxImager = new DocxImager();
-  //   await docxImager.load(resultFile);
-
-  //   await docxImager.insertImage({
-  //     img1: "https://w7.pngwing.com/pngs/895/199/png-transparent-spider-man-heroes-download-with-transparent-background-free.png",
-  //     img2: "https://onlinejpgtools.com/images/examples-onlinejpgtools/sunflower-0quality.jpg",
-  //   });
-  //   await docxImager.save(resultFile);
-
-  //   // response.header(
-  //   //   "Content-Type",
-  //   //   "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-  //   // );
-  //   response.attachment(resultFile);
-  //   // you can also access this file from http://localhost:3333/uploads/uploads/result.docx
-  // }
-  public async index({ response }: HttpContextContract) {
+  public async index({ response, request }: HttpContextContract) {
     const resultFile = Application.publicPath(`uploads/result.docx`);
-    const templateFile = Application.publicPath("uploads/image_docx.docx");
+    const templateFile = Application.publicPath("uploads/image_docx_1.docx");
 
     const content = fs.readFileSync(templateFile, "binary");
 
@@ -193,32 +135,96 @@ export default class IndexController {
       modules: [imageModule],
     });
 
-    // Render the document (Replace {first_name} by John, {last_name} by Doe, ...)
+    const case_cif_form_id = request.param("id");
+    const { case_form_input_id, token, agent_id } = await request.validate({
+      schema: schema.create({
+        case_form_input_id: schema.number(),
+        token: schema.string(),
+        agent_id: schema.number(),
+      }),
+    });
+
+    // console.log(case_cif_form_id);
+
+    const case_cif_form = await Database.query()
+      .from("case_cif_forms")
+      .where({ id: case_cif_form_id })
+      .firstOrFail();
+
+    const case_form_input = await Database.query()
+      .from("case_form_inputs")
+      .where("case_cif_form_id", "=", case_cif_form.id)
+      .where("submitted_by", "=", agent_id)
+      .firstOrFail();
+
+    const cif_form_template = await Database.query()
+      .from("cif_form_templates")
+      .where("case_cif_form_id", "=", case_cif_form.id)
+      .firstOrFail();
+
+    const cif_form_field_templates = await Database.query()
+      .from("cif_form_field_templates")
+      .where("cif_form_template_id", "=", cif_form_template.id);
+
+    // console.log(case_cif_form_id, case_form_input_id, token);
+
+    var images = await this.load_images(
+      case_cif_form_id,
+      case_form_input_id,
+      token
+    );
+
+    // return;
+
+    let cif_inputs: any[] = [];
+    cif_form_field_templates.map((t) => {
+      if (t.field_type === 7 && t.field_meta && t.field_meta.select_inputs) {
+        var obj = t.field_meta.select_inputs.find(
+          (ip: any) => ip.value == case_form_input.data[t.field_code]
+        );
+        console.log(t.field_meta.select_inputs);
+        console.log(case_form_input.data);
+        console.log(t.field_code);
+
+        cif_inputs.push({ label: `${t.field_label}`, value: obj.label });
+      } else {
+        cif_inputs.push({
+          label: `${t.field_label}`,
+          value: case_form_input.data[t.field_code],
+        });
+      }
+    });
+
+    var insured_photos: any[] = [];
+    for (const p of images?.data?.insured_photos) {
+      insured_photos.push({ url: await networkToBuffer(p.url) });
+    }
+
+    // console.log(insured_photos);
+
+    var id_proofs: any[] = [];
+    for (const p of images?.data?.id_proofs) {
+      id_proofs.push({ url: await networkToBuffer(p.url) });
+    }
+
+    // console.log(cif_inputs, insured_photos, id_proofs);
+
     doc.setData({
-      images: [
-        {
-          src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QIJBywfp3IOswAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAkUlEQVQY052PMQqDQBREZ1f/d1kUm3SxkeAF/FdIjpOcw2vpKcRWCwsRPMFPsaIQSIoMr5pXDGNUFd9j8TOn7kRW71fvO5HTq6qqtnWtzh20IqE3YXtL0zyKwAROQLQ5l/c9gHjfKK6wMZjADE6s49Dver4/smEAc2CuqgwAYI5jU9NcxhHEy60sni986H9+vwG1yDHfK1jitgAAAABJRU5ErkJggg==",
-        },
-        {
-          src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH5AcDDQ8FHr772QAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAo0lEQVQY02P8++/vfwYiABMDkYAFmeNfocXw6t5XBgYGBgYxJW6GjR3X4HKMMKv9K7QY3jz6hmKKqAI3w4a2q6gmvnn4lYGNi4WBhY2JgYWNiYGVgxm71aKKPAxsnMwMzFCFbJxMDEzMjJgK9T0kGV4/+MLAysHMwMLGyMDKycIgyi+B6UYGBgaGGecyGN58e8LAxsTBIM6jxBCv14VdIVXCEQAMjCktnpOJWwAAAABJRU5ErkJggg==",
-        },
-      ],
-      first_name: "Hipp",
-      last_name: "Edgar",
-      phone: "0652455478",
-      description: "New Website",
+      cif_inputs,
+      insured_photos,
+      id_proofs,
     });
 
     doc.render();
 
     const buffer = doc.getZip().generate({
       type: "nodebuffer",
-      // compression: DEFLATE adds a compression step.
-      // For a 50MB output document, expect 500ms additional CPU time
       compression: "DEFLATE",
     });
 
     fs.writeFileSync(resultFile, buffer);
+    response.attachment(resultFile);
+
     return "ok";
   }
 }
