8791efc4fb161787c173374d6d960924790492f3
Soumik Maity <soumik.maity@geogo.in>
2023-08-07 12:35:09 +0530

    Case details added

--- app/Services/RailsImage.ts
+++ app/Services/RailsImage.ts
@@ -30,6 +30,10 @@ export default async function ResolveImage(
     .from("cases")
     .where({ id: case_cif_form.case_id })
     .firstOrFail();
+  const customer = await Database.query()
+    .from("customers")
+    .where({ id: project.customer_id })
+    .firstOrFail();
   const case_form_input = await Database.query()
     .from("case_form_inputs")
     .where("case_cif_form_id", "=", case_cif_form.id)
@@ -42,6 +46,20 @@ export default async function ResolveImage(
   const cif_form_field_templates = await Database.query()
     .from("cif_form_field_templates")
     .where("cif_form_template_id", "=", cif_form_template.id);
+  const customer_post = await Database.query()
+    .from("post_offices")
+    .where("id", "=", customer.post_office_id)
+    .firstOrFail();
+  const report_sent_activities = await Database.query()
+    .from("activity_logs")
+    .where("loggable_type", "=", "Case")
+    .where("loggable_id", "=", project.id)
+    .where("event_type", "=", "stage_change");
+
+  const report_sent = report_sent_activities.filter(
+    (log) => log.event_data.to_stage === "report_submitted"
+  );
+
   var images = await load_images(case_cif_form_id, case_form_input_id);
   let cif_inputs: any[] = [];
   cif_form_field_templates.map((t) => {
@@ -73,6 +91,17 @@ export default async function ResolveImage(
   const signature = [{ url: images?.data?.signature?.url }];
   const summary = project?.summary || "";
 
+  function formatDate(dateString) {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    const offset = 5.5 * 60 * 60 * 1000;
+    return new Date(date.getTime() + offset).toLocaleString();
+  }
+
+  const { case_number, case_type, created_at } = project || {};
+  const { name, phone, address, city } = customer || {};
+  const { state } = customer_post || {};
+
   return {
     id_proofs,
     insured_photos,
@@ -83,6 +112,20 @@ export default async function ResolveImage(
     signature,
     cif_inputs,
     summary,
-    page_break: `<w:p><w:br w:type="page" /></w:p>`
+    app_no: case_number || "",
+    customer_name: name || "",
+    customer_phone: phone || "",
+    customer_address: address || "",
+    customer_city: city || "",
+    customer_state: state || "",
+    case_visit_type:
+      case_type === 1 ? "Scheduled" : case_type === 2 ? "Direct" : "",
+    case_allocation_date: formatDate(created_at),
+    case_done_date: formatDate(case_form_input?.created_at),
+    case_report_sent_date:
+      report_sent.length > 0
+        ? formatDate(report_sent[report_sent.length - 1]?.created_at)
+        : "",
+    page_break: `<w:p><w:br w:type="page" /></w:p>`,
   };
 }
